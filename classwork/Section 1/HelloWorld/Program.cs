7e0e606b67c02eedd3cb5ca0bbaec3fc1aa44be5
aaron.nesmith <aaron.nesmith@my.tccd.edu>
2019-08-26 18:56:19 -0500

    Talked about variables and types

--- classwork/Section 1/HelloWorld/Program.cs
+++ classwork/Section 1/HelloWorld/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 namespace HelloWorld
 {
+    // parameters are camelCase
 
     class Program
     { 
-        static void Main ( string[] args )
+        static void Main ( /*string[] args*/ )
         {
+            string name ="";
+
+           
+            // string if = "";
+
+            //Definitely assigned
+            //name = "Aaron";
+            string name2 = Console.ReadLine ();
+            //name2 = Console.Readline ();
+
+            name2 = name = "Aaron";
+            Console.WriteLine (name2);
             Console.WriteLine ("Hello World!");
+
+            int hours = 8;
+            double payRate = 15.25;
+
+            double totalPay = payRate * hours; 
+
+            string fullName = "Fred" + " " + "Jones";
+       
         }
     }
 }
